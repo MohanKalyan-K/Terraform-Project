12582f44f1102164a50259a9379d5d8b0839fa01
MohanKalyan-K <nishanthkalyan68@gmail.com>
2024-11-27 14:55:00 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -4,7 +4,7 @@ sudo yum install httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
 INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-echo "Public Instance 1" > /var/www/html/index.html
+echo "Public Instance: $INSTANCE_ID" > /var/www/html/index.html
 sudo yum install openssh -y 
 sudo systemctl start sshd
 
